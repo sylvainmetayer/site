3ab3f7d5e12a04e8daa1b8b75fae59813aad28d8
Sylvain METAYER <contact@sylvainmetayer.fr>
2018-11-24 17:31:08 +0100

    test

--- _plugins/environnment_variables.rb
+++ _plugins/environnment_variables.rb
@@ -6,12 +6,8 @@ module Jekyll
     class EnvironmentVariablesGenerator < Generator
   
       def generate(site)
-        puts "TEST"
-        puts ENV["IS_NETLIFY"]
-        puts ENV['DEPLOY_URL']
-        puts "END TEST"
         if ENV["IS_NETLIFY"]
-            site.config['url'] = ENV['DEPLOY_URL'].dup.chomp!("/")
+            site.config['url'] = ENV['DEPLOY_URL']
             puts "URL is now " + site.config['url']
         end
       end
