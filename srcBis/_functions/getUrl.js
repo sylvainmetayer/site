df3ba963ee399721bc0a2db539ac22be01a9fb10
Sylvain METAYER <s.metayer@groupeonepoint.com>
2020-01-30 14:25:47 +0100

    update

--- srcBis/_functions/getUrl.js
+++ srcBis/_functions/getUrl.js
@@ -3,6 +3,10 @@ module.exports = () => {
     return "https://sylvainmetayer.fr";
   }
 
+  if( process.env.DEPLOY_PRIME_URL) {
+    return process.env.DEPLOY_PRIME_URL
+  }
+
   // TODO Netlify preview deploy
 
   return "http://localhost:8080";
