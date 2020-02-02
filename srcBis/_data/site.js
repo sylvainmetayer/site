8af9e2b9be4472be81104a9977741deaa326c097
Sylvain METAYER <contact@sylvainmetayer.fr>
2020-02-03 00:28:05 +0100

    fix env

--- srcBis/_data/site.js
+++ srcBis/_data/site.js
@@ -1,7 +1,7 @@
-const environment = process.env.ELEVENTY_ENV ? (process.env.DEPLOY_PRIME_URL ? "dev" : process.env.ELEVENTY_ENV) : "production";
-
 const url = require("../_functions/getUrl");
 const authors = require("./authors")
+
+const productionUrl = "https://sylvainmetayer.fr";
 
 module.exports = {
   title: "Sylvain METAYER",
@@ -9,8 +9,8 @@ module.exports = {
   url: url(),
   baseUrl: "/",
   buildTime: new Date(),
-  environment: environment,
-  productionUrl: "https://sylvainmetayer.fr",
+  environment: url() === productionUrl ? "production" : "dev",
+  productionUrl: productionUrl,
   author: () => {
     return authors.sylvainmetayer;
   }
